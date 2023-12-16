ab371fe6bf331671c9bdde2bf9c3e298cec76f38
Nicolò D'Addabbo <nicolodaddabbo@gmail.com>
2023-12-16 14:53:20 +0100

    Home screen header

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -4,10 +4,10 @@ import Header from '../../components/Header/Header';
 
 export default function Home({ navigation }) {
     return (
-        // <View>
-        //     <View style={styles.headerContainer}>
-        //         <Header text="Header" button={false} />
-        //     </View>
+        <View>
+            <View style={styles.headerContainer}>
+                <Header text="Header" button={false} />
+            </View>
             <View style={styles.container}>
                 <AppButton
                     text="Click me!"
@@ -18,20 +18,23 @@ export default function Home({ navigation }) {
                     dimension={"small"}
                 />
             </View>
-        // </View>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  
     headerContainer: {
-      flex: 1,
-      alignItems: 'flex-start'
+        width: "100%",
+        height: "10%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    container: {
+        width: "100%",
+        height: "90%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
     }
-  });
+});
