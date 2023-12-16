7c9a7aab62f4fa6ce9f329509da65be2d9a97512
Patrik0724 <64145704+Patrik0724@users.noreply.github.com>
2023-12-16 14:02:40 +0100

    header fixed

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import { StyleSheet, View } from 'react-native';
 import AppButton from './components/AppButton/AppButton';
+import Header  from './components/Header/Header';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <AppButton
-        text="Click me!"
-        onPress={() => console.log("Button pressed!")}
-        color="red"
-        borderColor="red"
-        backgroundColor="white"
-        dimension={"small"}
-      />
+    <View>
+      <View style={styles.headerContainer}>
+        <Header text="Header" button={false}/>
+      </View>
+    
+      <View style={styles.container}>
+        <AppButton
+          text="Click me!"
+          onPress={() => console.log("Button pressed!")}
+          color="red"
+          borderColor="red"
+          backgroundColor="white"
+          dimension={"small"}
+        />
+      </View>
     </View>
   );
 }
@@ -23,4 +30,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  headerContainer: {
+    flex: 1,
+    alignItems: 'flex-start'
+  }
 });
